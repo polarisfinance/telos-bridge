2de897f8a7328cbf0caa494ad4e95269cb902ea4
kajaba <kajaba@gmail.com>
2024-02-05 21:18:21 +0100

    Logo,header and links

--- src/features/core/ui/CustomHtmlHead.tsx
+++ src/features/core/ui/CustomHtmlHead.tsx
@@ -10,11 +10,11 @@ interface CustomHtmlHeadProps {
 
 
 const defaultDescription = 'The LayerZero Telos bridge enables seamless asset transfers between Telos and other blockchains. Connect your source and destination wallets to facilitate the seamless transfer of assets between Telos and external ecosystems, alleviating the risk of wrapped asset bridges.';
-const keywords = 'telos, tlos, blockchain, crypto, message, transaction, omnichain, bridge';
+const keywords = 'polaris, polarisfinance, telos, tlos, blockchain, crypto, message, transaction, omnichain, bridge';
 
 export const CustomHtmlHead = (props: CustomHtmlHeadProps) => {
   const {description = defaultDescription, url} = props;
-  const metaTitle = 'Telos Bridge';
+  const metaTitle = 'Polaris Bridge';
   const theme = useTheme();
   return (
     <Head>
@@ -31,18 +31,13 @@ export const CustomHtmlHead = (props: CustomHtmlHeadProps) => {
       <meta charSet='utf-8' />
       <meta name='language' content='english' />
       <meta httpEquiv='content-type' content='text/html' />
-      <meta name='author' content={'Telos'} />
-      <meta name='designer' content={'Telos'} />
-      <meta name='publisher' content={'Telos'} />
+      <meta name='author' content={'Polaris'} />
+      <meta name='designer' content={'Polaris'} />
+      <meta name='publisher' content={'Polaris'} />
       <meta name='keywords' content={keywords} />
       <meta name='distribution' content='web' />
-      <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
-      <link rel='apple-touch-icon' sizes='16x16' href='/favicon-16x16.png' />
-      <link rel='apple-touch-icon' sizes='32x32' href='/favicon-32x32.png' />
-      <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-      <link rel='shortcut icon' href='/favicon.ico' />
+      <link rel='icon' type='image/png' href='https://app.polarisfinance.io/favicon.ico' />
+      <link rel='shortcut icon' href='https://app.polarisfinance.io/favicon.ico' />
       {/* <meta name='og:image' content={'/share.png'} />
       <link rel='mask-icon' color='#000000' href='/safari-pinned-tab.svg' />
       <link rel='manifest' href='/site.webmanifest' /> */}
@@ -50,7 +45,7 @@ export const CustomHtmlHead = (props: CustomHtmlHeadProps) => {
       <meta name='theme-color' content={theme.palette.primary.main ?? '#000000'} />
       <meta name='viewport' content='width=device-width, minimum-scale=1, initial-scale=1.0' />
       <meta name='twitter:card' content='summary' />
-      <meta name='twitter:site' content='@HelloTelos' />
+      <meta name='twitter:site' content='@PolarisFinance_' />
     </Head>
   );
 };
